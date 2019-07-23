ad84eb472e162846e35db6d089627cc5a59dd06c
Serge Muzyka <siarhei.muzyka@pennmedicine.upenn.edu>
2019-07-22 22:45:50 -0400

    Started with making code working. Then suddenly discovered that OpenGL development is no more supported on MacOSX Mojave. Should I start looking now at Vulkan?

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This project is a simple example of how to start with SDL library. Useful for te
 
 In this branch I keep an example of how to enumerate displays, video drivers and video modes that they support.
 Also the codebase contains `.vscode` folder which is set up in a way that allows g++ debugging with external terminal window.
+
+
+`sdl_opengl_triangle` branch
+============================
+The purpose of this branch is to show how to initialize SDL and create a window with Open GL context.
+When the app runs it shows a green triangle in a black window.
--- main.cpp
+++ main.cpp
@@ -1,58 +1,60 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <OpenGL/glext.h>
+#include <GLUT/glut.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define BITS_PER_PIXEL 32
 
 int main (int arggc, const char** argv) {
-    int width = 800;
-    int height = 600;
-    int bits_per_pixel = 32;
-    bool windowed = true;
-
-    SDL_Surface* pOGLSurface;
+    SDL_Window* pWindow;
+    SDL_GLContext glcontext;
+    SDL_Event event;
 
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cout<< "Failed to initialize SDL library." << std::endl;
+        std::cout << "Failed to initialize SDL library. Error: " << SDL_GetError() << std::endl;
         return -1;
     } 
 
-    int displays =  SDL_GetNumVideoDisplays();
-    int drivers = SDL_GetNumRenderDrivers();
-    std::cout << "Displays number: " << displays << std::endl;
-    for (int i = 0; i< displays; ++i)
-        std::cout << "\t\t" << SDL_GetDisplayName(i) << std::endl;
-
-    std::cout << "Drivers number: " << drivers << std::endl;
-
-    for(int driver_index = 0; driver_index < drivers; ++driver_index) {
-        SDL_RendererInfo info;
-        SDL_GetRenderDriverInfo(driver_index, &info);
-        std::cout << "-------------------------------------\n";
-        std::cout << "Driver: " << info.name << std::endl;    
-        
-        std::cout << "FLAGS: ";
-        if (info.flags & SDL_RENDERER_SOFTWARE) std::cout << "SDL_RENDERER_SOFTWARE ";
-        if (info.flags & SDL_RENDERER_ACCELERATED) std::cout << "SDL_RENDERER_ACCELERATED ";
-        if (info.flags & SDL_RENDERER_PRESENTVSYNC) std::cout << "SDL_RENDERER_PRESENTVSYNC ";
-        if (info.flags & SDL_RENDERER_TARGETTEXTURE) std::cout << "SDL_RENDERER_TARGETTEXTURE ";
-        std::cout << std::endl;
-
-        std::cout << "Supported formats: " << std::endl;
-        for(int i = 0; i < info.num_texture_formats; ++i) {
-            std::cout << "\t\t" << SDL_GetPixelFormatName(info.texture_formats[i]) << std::endl;
-        }
-
-        std::cout << "Max texture width: " << info.max_texture_width << std::endl;
-        std::cout << "Max texture height: " << info.max_texture_height << std::endl;
+    pWindow = SDL_CreateWindow( "SDL + OpenGL rock",
+                                SDL_WINDOWPOS_CENTERED,
+                                SDL_WINDOWPOS_CENTERED,
+                                WINDOW_WIDTH,
+                                WINDOW_HEIGHT,
+                                SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    if (!pWindow) {
+        std::cout << "Failed to create window. Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();    
+        return -1;
     }
 
-    SDL_Rect rect;
-    SDL_GetDisplayBounds(0, &rect);
-    std::cout << "Bounds x= " << rect.x << ", ";
-    std::cout << "y= " << rect.y << ", ";
-    std::cout << "w= " << rect.w << ", ";
-    std::cout << "h= " << rect.h << std::endl;
-    //SDLGetVideoInfo
+    glcontext = SDL_GL_CreateContext(pWindow);
+    if (!glcontext) {
+        std::cout << "Failed to create Open GL context. Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(pWindow);
+        SDL_Quit();
+        return -1;
+    }
 
+    //glClearColor(0,0,0,1);
+    while (1) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT) break;
+        else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) break;
 
+        //SDL_SetRenderDrawColor(g_pRenderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
+        //SDL_RenderClear(g_pRenderer);
+        //SDL_RenderPresent(g_pRenderer);
+
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(pWindow);
+    }
+    
+    SDL_GL_DeleteContext(glcontext);
     SDL_Quit();
     return 0;
 }
